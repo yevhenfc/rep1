dc9047a3298d7022d8e0bfae86375d6676222c67
yevhen <yevhenfc@gmail.com>
2022-06-05 19:03:47 +0300

    obj 20220605

--- js/index.js
+++ js/index.js
@@ -1,151 +1,69 @@
-// let ar = ['a', 'b', 'c', 'd', 'e']
-// let i=0;
-// while (i++ < ar.length) console.log(i-1,ar[i-1]); 
-
-// let ar = [1, 2, 3, 4, 5]
-// // while (i++ < ar.length) 
-// for (i = ar.length-1; i>=0; i--) {
-//         // ar[i-1]*=2;
-//         console.log(i, ar[i], ar[i]%2 ? ar[i]:0, ar[i]%2!=false ? 0:ar[i]);
-//     } 
-
-// let arr = [10, 20, 50, 100, 0, 0, 300, 0, 500, 600, 0, 0, ]
-// for (i = 0; i < arr.length; i++) {arr[i] == 0 ? console.log(i) : true}
-// let res=0;
-// for (i = 0; i < arr.length; i++) {arr[i] == 0 ? res++ : true};
-// до первого нуля
-// for (i = 0; i < arr.length; i++) {
-//     if (arr[i] == 0)  break;
-//     console.log(arr[i]); 
-//     res++
-// };
-// console.log(res);
-
-// сумма соседених эллементов
-// 1+2 2+3
-// 1+2 3+4
-// let sum1=[];
-// for (i = 0; i < (arr.length - 1); i++) console.log(i, arr[i], arr[i+1], arr[i]+arr[i+1])
-// for (i = 0; i < (arr.length - 1); i+=2) console.log(i, arr[i], arr[i+1], arr[i]+arr[i+1])
-// for (i = 0, j=0; i < (arr.length - 1); i+=2,j++) {
-//     sum1[j]=arr[i]+arr[i+1];
-// }
-// console.log(sum1);
-
-//let arr = [4, 0, -1, -5, 3.14, 0, 0, 100, -4, 2.45, 4, 0, 4];
-// find and show negative values
-// for (i = 0; i < arr.length; i++) arr[i] < 0 ? console.log(arr[i]):1;
-// function funShowNegVal(userArr) {
-//     for (i = 0; i < userArr.length; i++) userArr[i] < 0 ? console.log(userArr[i]):1;
-// };
-// funShowNegVal(arr);
-
-
-// qty of negative ellements
-// for (i = 0, j=0; i < arr.length; i++) arr[i] < 0 ? (console.log(arr[i]),j++):1;
-// console.log(j);
-// function funQtyNegVal(userArr) {
-//     for (i = 0, j=0; i < arr.length; i++) arr[i] < 0 ? (console.log(arr[i]),j++):1;
-//     return j;
-// };
-// console.log(funQtyNegVal(arr));
-
-// save results to another array
-// let res =[];
-// for (i = 0, j=0; i < arr.length; i++) arr[i] < 0 ? (res[j]=arr[i],j++):1;
-// console.log(res);
-// function funSaveResToArray(userArr) {
-//     let res =[];
-//     for (i = 0, j=0; i < userArr.length; i++) userArr[i] < 0 ? (res[j]=userArr[i],j++):1;
-//     return(res);
-// };
-// console.log(funSaveResToArray(arr));
-
-
-//switch negative to zero
-// for (i = 0; i < arr.length; i++) arr[i] < 0 ? arr[i]=0:1;
-// console.log(arr);
-// function funNegativeToZero(userArr) {
-//     for (i = 0; i < userArr.length; i++) userArr[i] < 0 ? userArr[i]=0:1;
-//     return userArr;
-// };
-// console.log(funNegativeToZero(arr));
-// return elements less limit
-// function fArrayLim(userArr, lim = prompt('enter lim', 10)) {
-// for (i = 0; i < userArr.length; i++) userArr[i] <= lim ? console.log(userArr[i]) : 1
-// };
-// function fArraySumPos(userArr) {
-//     let res = 0;
-//     for (i = 0; i < userArr.length; i++) userArr[i] >= 0 ? res += userArr[i]: 1
-//    console.log(res);    
-// }; 
-// fArraySumPos(arr);
-// function fArraySqr(userArr) {
-//     let res = 0;
-//     for (i = 0; i < userArr.length; i++) userArr[i] = userArr[i]**2;    
-//     return(userArr);
-// }; 
-// console.log(fArraySqr(arr));
-
-// function negat(userArr) {
-//     let res1 = [];
-//     for (i = 0, j = 0; i < userArr.length; i++) userArr[i] < 0 ? (res1[j] = userArr[i], j++) : false;
-//     return(res1);
-// }; 
-
-// function sumArr(userArr) {
-//     let res = 0;
-//     for (i = 0; i < userArr.length; i++) userArr[i] < 0 ? res += userArr[i] : 0;
-//     return(res);
-// }
-
-// function fArraySqr(userArr){
-//         let qwe = negat(userArr) , sumw;    
-//         sumw = sumArr(qwe);
-//         return(sumw);
-//     }; 
-
-//     console.log(fArraySqr(arr));
-// ******************************************************************************************************
-// ******************************************************************************************************
-let arr = [4, 0, -1, -5, 3.14, 0, 0, 100, -4, 2.45, 4, 0, 4];
-//find words > 4 symbols
-const words = ['hello','Vasya','Oh','fox','JS','array'];
-
-let checkLen = (item) => item.length > 4 ? true : false;
-
-let res = [], i = 0;
-//words.forEach(function(item, j, arr) {item.length > 4 ? (res[i] = item, i++) : false});
-// words.forEach(checkLen(item) == true ? (res[i] = item, i++) : false);
-
-// console.log(res);
-// ******************************************************************************************************
-/*
-1. Сумма нечетных элементов массивов
-2. Вывести индексы нулевых эелементов массива
-3. 2 массива соединить и преобразовать в строку
-4. отфильтровать массив, удалив все нули
-*/ 
-
-//1. Сумма нечетных элементов массивов
-//1-1
-// let a = [1, 2, 3, 4, 5, 6];
-// let resOdd = a.filter(isOdd = (value) => value % 2 == 1);
-// let resSum1 = resOdd.reduce(sumNum = (a, b) => a + b)
-// console.log(resSum1);
-// //1-2
-// let resSum2 = (a) => a.filter((el, i ) => (el%2)).reduce((SumNum, val) => SumNum + val);
-// console.log(resSum2(a));
-// //1-3
-// let resSum3 = (a) => {
-//     let res = 0;
-//     a.forEach(el => el %2 != 0 ? res += el : 0);
-//     return res;
-// };
-// console.log(resSum3(a));
-
-//2. Вывести индексы нулевых эелементов массива
-let a = [1, 2, 0, 3, 4, 5, 0, 6];
-let res = (a) => a.forEach(el, i) => {
+// let work = {
+//     year: 2015,
+//     salary: 100,
+//     stag: 0, 
     
-});
+//     calcStag: function () {
+//         this.stag = (new Date().getFullYear()) - this.year
+//         return this.stag;
+//     },
+    
+//     setSalary: function(pr = 15) {
+//         if (this.stag >= 3) this.salary *= (1 + pr / 100)
+//         return this.salary
+//     }
+// }
+// ;
+// console.log(work.calcStag());
+// console.log(work.setSalary());
+
+
+// let arrT = {
+//     start: 0,
+//     end: 0,
+//     arr: [],
+//     genArr: function(start, n, step){
+//         let res = [], i;
+//         this.start = start;
+//         this.end = start + (n - 1) * step;
+//         for (i = 0; i < n; i++) res[i] = (i + 1)  * step;
+//         return res;
+//     },
+
+//     setArr: function(start, n, step){
+//         let res = [], i;
+//         this.start = start;
+//         this.end = start + (n - 1) * step;
+//         for (i = 0; i < n; i++)  res.push((i + 1)  * step); //res[i] = (i + 1)  * step;
+//         this.arr = res;
+//     },
+// }; 
+    
+// // console.log(arrT.genArr(10, 5, 10));
+// console.log(arrT.setArr(10, 5, 10));
+
+
+objBuy = {
+    totalSum: 0,
+    sale: 0,
+    setSum: function(value) {return this.totalSum=value}, 
+    addSale: function(){
+        switch(true){
+            case this.totalSum >= 1000: this.totalSum *=.95; this.sale = 5; break;
+            case this.totalSum >= 500:  this.totalSum *=.97; this.sale = 3; break;
+            default:                                         this.sale = 0; break;
+        }
+    },
+};
+
+objBuy.setSum(100);
+objBuy.addSale();
+console.log(objBuy);
+
+objBuy.setSum(750);
+objBuy.addSale();
+console.log(objBuy);
+
+objBuy.setSum(1500);
+objBuy.addSale();
+console.log(objBuy);
