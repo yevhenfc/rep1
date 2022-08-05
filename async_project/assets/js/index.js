6cb12fe41a2138fb2de7bcec76f7b0954d25cf5b
yevhen <yevhenfc@gmail.com>
2022-08-05 08:57:45 +0300

    promise

--- async_project/assets/js/index.js
+++ async_project/assets/js/index.js
@@ -1,31 +1,46 @@
-const btn = document.getElementById('loadUsers');
+// const promise1 = new Promise(test);
 
-const store = {
-    users: null,
-    isFetching: false,
-    error: null,
-};
+// function test(resolve, reject) {
+//     console.log('Hello, Aaada!');
+//     if(reject) {new Error('Hello, Aaada!')}    
+// };
 
-btn.onclick = function () {
-    fetch('./assets/data/users1.json').then(loadResolve).then(logData).catch(loadReject);
-};
+let promiseGetNumMore50 = new Promise(getNumMore50);
 
-//data to console
-function logData(data){
-    store.isFetching = false;
-    store.users = data;
-    console.log(data);
+function getNumMore50(resolve, reject) {
+    const num = Math.round(Math.random()* 100);
+    console.log(num);
+    num >= 50 ? resolve(num) : reject (new Error(`Reject. Number ${num} failed`));
 }
 
-// good!
-function loadResolve(response){
-    store.isFetching = true;
-    return response.json();
-}
+// const btn = document.getElementById('loadUsers');
+
+// const store = {
+//     users: null,
+//     isFetching: false,
+//     error: null,
+// };
+
+// btn.onclick = function () {
+//     fetch('./assets/data/users1.json').then(loadResolve).then(logData).catch(loadReject);
+// };
+
+// //data to console
+// function logData(data){
+//     store.isFetching = false;
+//     store.users = data;
+//     console.log(data);
+// }
+
+// // good!
+// function loadResolve(response){
+//     store.isFetching = true;
+//     return response.json();
+// }
 
  
-// error , bad ((
-function loadReject(response){
-    store.error = new Error(`${response.status} ${response.statusText}`);
-    console.log(store.error);
-}
+// // error , bad ((
+// function loadReject(response){
+//     store.error = new Error(`${response.status} ${response.statusText}`);
+//     console.log(store.error);
+// }
